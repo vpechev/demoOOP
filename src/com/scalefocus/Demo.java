d17742918e573b2a6d79b8df0af41c3d16435391
vpechev <vladimir.pechev@abv.bg>
2018-09-10 20:53:29 +0300

    add demo

--- src/com/scalefocus/Demo.java
+++ src/com/scalefocus/Demo.java
@@ -1,8 +1,20 @@
 package com.scalefocus;
 
+import com.scalefocus.common.Color;
+import com.scalefocus.common.EngineType;
+import com.scalefocus.domain.Car;
+
 public class Demo {
 
     public static void main(String[] args) {
-	// write your code here
+        Car dieselCar = new Car(EngineType.DIESEL, 1600, 90, Color.YELLOW);
+        System.out.println("Firstly created diesel car: " + dieselCar);
+
+        // Already it's impossible to access the property directly.
+        //dieselCar.engineType = EngineType.ELECTRICS;
+
+        dieselCar.setEngineType(EngineType.ELECTRICS);
+
+        System.out.println("Car after modify: " + dieselCar);
     }
 }
